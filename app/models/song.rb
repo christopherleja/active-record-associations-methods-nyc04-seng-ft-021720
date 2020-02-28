f7b6f746378d24dae6ce120d65e8cd14d3056ee5
Christopher Leja <christopher.leja@gmail.com>
2020-02-28 18:52:22 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,8 +7,8 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    self.artist.find_or_create(name: "Drake")
-    
+    drake = Artist.find_or_create_by(name: "Drake")
+    self.artist = drake
     end
   end
 end
